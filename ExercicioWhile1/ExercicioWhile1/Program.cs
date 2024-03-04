43ac46e86e8c2b894fb3e79a8413df16fd52c510
Luiz Mendes <120228527+LuizMendes1997@users.noreply.github.com>
2024-03-04 14:01:21 -0300

    Add files via upload

---
+++ ExercicioWhile1/ExercicioWhile1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+            Console.WriteLine("Digite sua senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002) {
+            
+                Console.WriteLine("Senha Incorreta!");
+                Console.WriteLine("Tente Novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Finalizado!!!!");
+            
+        }
+    }
+}
---
+++ ExercicioWhile2/ExercicioWhile2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+            int alcool = 0;  int diesel = 0; int gasolina =0;
+            Console.WriteLine("Qual abastecimento deve ser incluido: ");
+            Console.WriteLine("1. Alcool ");
+            Console.WriteLine("2. Diesel ");
+            Console.WriteLine("3. Gasolina ");
+            Console.WriteLine("4. Fim ");
+            opcao = int.Parse(Console.ReadLine());
+            
+                while (opcao != 4)
+                    {
+                        if (opcao == 1)
+                        {
+                            alcool = alcool + 1;
+                        }
+                        else if (opcao == 2)
+                        {
+                            diesel = diesel + 1;
+                        }
+                        else if (opcao == 3)
+                        {
+                            gasolina = gasolina + 1;
+                        }
+                        Console.WriteLine("Qual abastecimento deve ser incluido: ");
+                        Console.WriteLine("1. Alcool ");
+                        Console.WriteLine("2. Diesel ");
+                        Console.WriteLine("3. Gasolina ");
+                        Console.WriteLine("4. Fim ");
+                        opcao = int.Parse(Console.ReadLine());
+                    }
+                
+                          
+            Console.WriteLine("Muito Obrigado");
+            Console.WriteLine("Alcool " + alcool);
+            Console.WriteLine("Diesel " + diesel);
+            Console.WriteLine("Gasoline " + gasolina);
+        }
+    }
+}
